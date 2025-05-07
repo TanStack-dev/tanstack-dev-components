73f5aa600828af09c55677d46cdda6679894cf8e
yanqi.zong <yzong@turo.com>
2025-05-06 20:37:48 -0700

    fix: fix dev

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: ['src/__tests__', 'src/**/*.test.tsx'],
+      outDir: 'dist',
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
